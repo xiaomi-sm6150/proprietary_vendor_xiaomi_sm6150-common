6522dfef1eff4f578e3a17bfa239c7329e79a14a
basamaryan <basam.aryan@gmail.com>
2025-01-02 18:08:30 +0100

    sm6150-common: Declare WFD lib as symlink during extraction
    Change-Id: I7e4f94f28b49cda6a2c277e569ad67a25191d023

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -492,6 +492,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
     system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
+    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
     vendor_lib_libq3dtools_adreno_so \

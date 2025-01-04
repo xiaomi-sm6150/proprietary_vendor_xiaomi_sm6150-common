c7afef00bfc24e82d302694c67fa87d81e37cd0e
basamaryan <basam.aryan@gmail.com>
2025-01-04 12:35:37 +0100

    sm6150-common: Regenerate
    Change-Id: I45195ca087f2a8d2c779692ca73f0d564d88a3a7

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -132,7 +132,7 @@ PRODUCT_PACKAGES += \
     libllvm-glnext \
     libllvm-qcom \
     libmdmdetect \
-    libmmosal \
+    libmmosal_vendor \
     libqmi \
     libqmi_cci \
     libqmi_client_helper \
@@ -191,7 +191,7 @@ PRODUCT_PACKAGES += \
     libwfdutils_proprietary \
     libasphere \
     libshoebox \
-    vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     com.qualcomm.qti.dpm.api@1.0_vendor \
     com.qualcomm.qti.imscmservice@2.0 \
